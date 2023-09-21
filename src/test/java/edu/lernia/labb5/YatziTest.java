1866fcc2072d62d98bdfefd69c88d7648837a240
Anders Myrman <amyhrman@gmail.com>
2023-09-21 14:42:06 +0200

    Refactor test and add @BeforeEach

--- src/test/java/edu/lernia/labb5/YatziTest.java
+++ src/test/java/edu/lernia/labb5/YatziTest.java
@@ -4,36 +4,46 @@ import static org.assertj.core.api.Assertions.*;
 
 import java.lang.reflect.Field;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class YatziTest {
 
-    @Test
-    void whenAllDiceMatches_thenAssertThatUserGotYatzi() {
-        // Arrange: Create a YatziGame object and an array of Die objects with the same value
+  // Declare the common objects as fields
+  private YatziGame yatziGame;
+  private Die[] dice;
 
-        YatziGame yatziGame = new YatziGame();
+  // Use the @BeforeEach annotation to set up the common objects before each test
+  @BeforeEach
+  public void setUp() {
+    // Create a YatziGame object
+    yatziGame = new YatziGame();
 
-        // Act: Set all dice values to 6
-        Die[] dice = new Die[5];
-        int yatziValue = 6;
-        for (int i = 0; i < dice.length; i++) {
-            dice[i] = new Die(yatziValue);
-        }
+    // Create an array of Die objects
+    dice = new Die[5];
 
-        // Use reflection to access the private field die and set its value to the dice array
-        try {
-            Field dieField = YatziGame.class.getDeclaredField("die");
-            dieField.setAccessible(true);
-            dieField.set(yatziGame, dice);
-          } catch (NoSuchFieldException | IllegalAccessException e) {
-            e.printStackTrace();
-          }
-      
-          // Act: Call the userGotYatzi method
-          boolean result = yatziGame.userGotYatzi();
-      
-          // Assert: Check if the result is true
-          assertThat(result).isTrue();
-        }
+    // Use reflection to access the private field die and set its value to the dice array
+    try {
+      Field dieField = YatziGame.class.getDeclaredField("die");
+      dieField.setAccessible(true);
+      dieField.set(yatziGame, dice);
+    } catch (NoSuchFieldException | IllegalAccessException e) {
+      e.printStackTrace();
     }
+  }
+
+  @Test
+  void whenAllDiceMatches_thenAssertThatUserGotYatzi() {
+    // Arrange: Set all dice values to 6
+    int yatziValue = 6;
+    for (int i = 0; i < dice.length; i++) {
+      dice[i] = new Die(yatziValue);
+    }
+
+    // Act: Call the userGotYatzi method
+    boolean result = yatziGame.userGotYatzi();
+
+    // Assert: Check if the result is true
+    assertThat(result).isTrue();
+  }
+}
